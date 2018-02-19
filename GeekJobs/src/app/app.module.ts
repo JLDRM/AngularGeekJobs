c6b57c5525b1d28bca626b78c623a2a8b8e902e0
marta <rulex12@gmail.com>
2018-02-19 11:16:04 +0100

    Componente form empresa

--- GeekJobs/src/app/app.module.ts
+++ GeekJobs/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RegistroUsuarioComponent } from './registro-usuario/registro-usuario.component';
+import { FormEmpresaComponent } from './form-empresa/form-empresa.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegistroUsuarioComponent
+    RegistroUsuarioComponent,
+    FormEmpresaComponent
   ],
   imports: [
     BrowserModule,NgbModule.forRoot()
