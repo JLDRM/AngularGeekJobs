d4e9458428df2da2cd1cca2ff290b1475633582a
marta <rulex12@gmail.com>
2018-02-19 16:25:49 +0100

    Fallos

--- GeekJobs/src/app/form-empresa/form-empresa.component.ts
+++ GeekJobs/src/app/form-empresa/form-empresa.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, OnChanges } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
-import { FormAltaofertaComponent } from '../form-altaoferta/form-altaoferta.component';
+import { Empresa } from '../../models/empresa';
+
 
 @Component({
   selector: 'app-form-empresa',
@@ -9,8 +10,12 @@ import { FormAltaofertaComponent } from '../form-altaoferta/form-altaoferta.comp
 })
 export class FormEmpresaComponent implements OnInit {
   empresaForm: FormGroup;
+  empresa: Empresa;
+  nom_ePattern: "[A-Z ÁÉÍÓÚ][a-z áéíóú]*";
+  ema_ePattern: "[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$";
+  pass_ePattern: "^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,10}$";
 
-<<<<<<< HEAD
+
   constructor(private fb: FormBuilder) {
     this.createForm();
   }
@@ -24,45 +29,57 @@ export class FormEmpresaComponent implements OnInit {
       empresa_emp: ['', Validators.required],
       nif_emp: ['', Validators.required],
       telefono_emp: ['', Validators.required],
-      stack_emp: [[], Validators.required],
+      stack_emp: [],
       chk_emp: ['', Validators.required]
     })
   }
-=======
-  constructor(
-    public nombre: string,
-    public apellido: string,
-    public email: string,
-    public contraseña: string,
-    public empresa: string,
-    public nif: string,
-    public telefono: string,
-    public stack: Array<any>
-  ) { }
->>>>>>> 3299ababc1e47c5709e74a688e0fd34906b913d2
 
   ngOnInit(): void {
     this.empresaForm = new FormGroup({
-      'nombre_emp': new FormControl(this.empresa.nombre_emp,[
+      'nombre_emp': new FormControl(this.empresa.nombre_emp, [
         Validators.required,
         Validators.minLength(3),
+        Validators.pattern(this.nom_ePattern)
+      ]),
+      'apellido_emp': new FormControl(this.empresa.nombre_emp, [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.pattern(this.nom_ePattern)
+      ]),
+      'email_emp': new FormControl(this.empresa.nombre_emp, [
+        Validators.required,
+        Validators.pattern(this.ema_ePattern)
+      ]),
+      'password_emp': new FormControl(this.empresa.nombre_emp, [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.pattern(this.pass_ePattern)
+      ]),
+      'cpassword_emp': new FormControl(this.empresa.nombre_emp, [
+        Validators.required,
+        Validators.minLength(6),
+  
+      ]),
+      'empresa_emp': new FormControl(this.empresa.nombre_emp, [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+      'nif_emp': new FormControl(this.empresa.nombre_emp, [
+        Validators.required,
 
+      ]),
+      'telefono_emp': new FormControl(this.empresa.nombre_emp, [
+        Validators.required,
+
+      ]),
+      'stack_emp': new FormControl(this.empresa.nombre_emp, [
+      ]),
+      'chk_emp': new FormControl(this.empresa.nombre_emp, [
+        Validators.required,
       ])
     });
   }
-  ngOnChanges(){
-    this.empresaForm.setValue({
-      nombre_emp: this.nombre_emp,
-      apellido_emp: this.nombre_emp,
-      email_emp: ['', Validators.required],
-      password_emp: ['', Validators.required],
-      cpassword_emp: ['', Validators.required],
-      empresa_emp: ['', Validators.required],
-      nif_emp: ['', Validators.required],
-      telefono_emp: ['', Validators.required],
-      stack_emp: [[], Validators.required],
-      chk_emp: ['', Validators.required]
-    })
+  ngOnChanges() {
   }
 
 }
---
+++ GeekJobs/src/models/empresa.ts
@@ -0,0 +1,13 @@
+export class Empresa {
+    nombre_emp: string;
+    apellido_emp: string;
+    email_emp: string;
+    password_emp: string;
+    cpassword_emp: string;
+    empresa_emp: string;
+    nif_emp: string;
+    telefono_emp: string;
+    stack_emp: string;
+    chk_emp: string;
+  }
+
