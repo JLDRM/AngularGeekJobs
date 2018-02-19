9e28f1fe8ff843d64825e7e98598c8128c0866d6
Johnny Duque <johnnyduque93@gmail.com>
2018-02-19 11:23:32 +0100

    header y footer accepted

--- GeekJobs/src/app/app.module.ts
+++ GeekJobs/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RegistroUsuarioComponent } from './registro-usuario/registro-usuario.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegistroUsuarioComponent
+    RegistroUsuarioComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,NgbModule.forRoot()
