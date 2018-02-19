4e99ab0495c896ea0de4a5434daf1fddc39dccd0
Josep López de Recalde Martorell <jldr244@gmail.com>
2018-02-19 16:18:11 +0100

    Reparo Errores --- ROAD TO TESTING

--- GeekJobs/src/app/form-usuario/form-usuario.component.ts
+++ GeekJobs/src/app/form-usuario/form-usuario.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+
+import { Geek } from '../../models/geek';
 
 
 @Component({
@@ -7,44 +9,68 @@ import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms'
   templateUrl: './form-usuario.component.html',
   styleUrls: ['./form-usuario.component.css']
 })
+
 export class FormUsuarioComponent implements OnInit {
 
   usuarioForm: FormGroup;
+  usuario: Geek;
 
-  constructor(private fb: FormBuilder) { 
+  constructor(private fb: FormBuilder) {
     this.createForm();
   }
 
   createForm() {
-    this.usuarioForm = this.fb.group ({
-    nombre:'',
-    apellido:'',
-    email:'',
-    contraseña:'',
-    conficontraseña:'',
-    telef:'',
-    habili:'',
-    hobby:'',
-    cv:''
+    this.usuarioForm = this.fb.group({
+      nombre: '',
+      apellido: '',
+      email: '',
+      password: '',
+      confirmacionpass: '',
+      telefono: '',
+      habilidades: '',
+      hobby: '',
+      cv: ''
 
-    }),
-    
-  ngOnInit() : void {
-    this.usuarioForm = new FormGroup({
-      'nombre': new FormControl(this.hero.name, [
-        Validators.required,
-        Validators.minLength(4),
-        forbiddenNameValidator(/bob/i) // <-- Here's how you pass in the custom validator.
-      ]),
-      'apellido': new FormControl(this.hero.alterEgo),
-      'email': new FormControl(this.hero.power, Validators.required)
-      'contraseña': new FormControl(this.hero.power, Validators.required)
-      'conficontraseña': new FormControl(this.hero.power, Validators.required)
-      'telef': new FormControl(this.hero.power, Validators.required)
-      'habili': new FormControl(this.hero.power, Validators.required)
-      'hobby': new FormControl(this.hero.power, Validators.required)
-      'cv': new FormControl(this.hero.power, Validators.required)
-    });
+    })
   }
 
-}
+    ngOnInit(): void {
+      this.usuarioForm = new FormGroup({
+        'nombre': new FormControl(this.usuario.nombre, [
+          Validators.required,
+          Validators.minLength(4),
+        ]),
+        'apellido': new FormControl(this.usuario.email,[
+          Validators.required,
+          Validators.minLength(4),
+        ]),
+        'email': new FormControl(this.usuario.email,[
+          Validators.required,
+          Validators.minLength(4),
+        ]),
+        'contraseña': new FormControl(this.usuario.password,[
+          Validators.required,
+          Validators.minLength(4),
+        ]),
+        'conficontraseña': new FormControl(this.usuario.confirmacionpass,[
+          Validators.required,
+          Validators.minLength(4),
+        ]),
+        'telefono': new FormControl(this.usuario.telefono,[
+          Validators.required,
+          Validators.minLength(4),
+        ]),
+        'habilidades': new FormControl(this.usuario.habilidades,[
+          Validators.required,
+          Validators.minLength(4),
+        ]),
+        'hobby': new FormControl(this.usuario.hobby,[
+          Validators.required,
+          Validators.minLength(4),
+          Validators.pattern('^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$')
+        ]),
+        'cv': new FormControl()
+      })
+    }
+
+  }
--- GeekJobs/src/models/geek.ts
+++ GeekJobs/src/models/geek.ts
@@ -1,4 +1,17 @@
 export class Geek {
-    id: number;
-    name: string;
+    nombre: string
+	apellidos: string
+	fotoPerfil: string
+	descripcion: string
+	habilidadesPrincipales: string
+	habilidades: string
+	portafolio: string
+	experiencia: string
+	formacion: string
+	email: string
+    password: string
+    confirmacionpass:string
+	telefono: string
+    hobby: string
+    
   }
