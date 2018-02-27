e5cfdebd05bfe789b650a7267b15eeac682004b5
Johnny Duque <johnnyduque93@gmail.com>
2018-02-27 18:16:04 +0100

    favicon solventado

--- src/app/home-geek/home-geek.component.ts
+++ src/app/home-geek/home-geek.component.ts
@@ -11,23 +11,14 @@ export class HomeGeekComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-<<<<<<< HEAD
-    $('#scroll').click(function(e){				
+    $('#scroll').click(function (e) {
       e.preventDefault();		//evitar el eventos del enlace normal
-      var strAncla=$(this).attr('href'); //id del ancla
-        $('body,html').stop(true,true).animate({				
-          scrollTop: $(strAncla).offset().top
-        },1000);
-=======
-    // $('button').click(function(e){				
-    //   e.preventDefault();		//evitar el eventos del enlace normal
-    //   var strAncla=$(this).attr('href'); //id del ancla
-    //     $('body,html').stop(true,true).animate({				
-    //       scrollTop: $(strAncla).offset().top
-    //     },1000);
->>>>>>> 0013497d9128f2c03692fabae3b31f391dca3cf6
-      
-    // });
+      var strAncla = $(this).attr('href'); //id del ancla
+      $('body,html').stop(true, true).animate({
+        scrollTop: $(strAncla).offset().top
+      }, 1000);
+
+    });
   }
 
 }
