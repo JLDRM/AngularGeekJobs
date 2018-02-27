15b20516eb3c7f431199dd2a7c4c52688f802ac6
Johnny Duque <johnnyduque93@gmail.com>
2018-02-27 17:07:36 +0100

    Cambios finales de las homes!

--- src/app/home-empresa/home-empresa.component.ts
+++ src/app/home-empresa/home-empresa.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-home-empresa',
@@ -10,6 +11,13 @@ export class HomeEmpresaComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    $('button').click(function(e){				
+      e.preventDefault();		//evitar el eventos del enlace normal
+      var strAncla=$(this).attr('href'); //id del ancla
+        $('body,html').stop(true,true).animate({				
+          scrollTop: $(strAncla).offset().top
+        },1000);
+      
+    });
   }
-
 }
