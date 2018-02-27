ac452a823ecc6efebcc0d0d75bc11f778f28f53d
Josep López de Recalde Martorell <jldr244@gmail.com>
2018-02-27 17:07:53 +0100

    Push últimos cambios LOGIN == JOSEP ==

--- src/app/home-geek/home-geek.component.ts
+++ src/app/home-geek/home-geek.component.ts
@@ -11,14 +11,14 @@ export class HomeGeekComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    $('button').click(function(e){				
-      e.preventDefault();		//evitar el eventos del enlace normal
-      var strAncla=$(this).attr('href'); //id del ancla
-        $('body,html').stop(true,true).animate({				
-          scrollTop: $(strAncla).offset().top
-        },1000);
+    // $('button').click(function(e){				
+    //   e.preventDefault();		//evitar el eventos del enlace normal
+    //   var strAncla=$(this).attr('href'); //id del ancla
+    //     $('body,html').stop(true,true).animate({				
+    //       scrollTop: $(strAncla).offset().top
+    //     },1000);
       
-    });
+    // });
   }
 
 }
