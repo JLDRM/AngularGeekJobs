b2caeb3d5695b3df47ee4ff0546c4da78044f27b
GitPusher <kamalonu@amail.club>
2018-02-28 17:08:50 +0100

    SomeMinorChanges

---
+++ src/app/form-perfil-usuario/form-perfil-usuario.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-perfil-usuario',
+  templateUrl: './form-perfil-usuario.component.html',
+  styleUrls: ['./form-perfil-usuario.component.css']
+})
+export class FormPerfilUsuarioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
