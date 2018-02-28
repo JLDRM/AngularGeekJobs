a7e8af88b8bb8a57e0cb7b619ab4acafaba16bb2
Johnny Duque <johnnyduque93@gmail.com>
2018-02-28 16:46:45 +0100

    Conflictos arreglados registros scrolls.

--- src/app/registro-empresa/registro-empresa.component.ts
+++ src/app/registro-empresa/registro-empresa.component.ts
@@ -11,19 +11,11 @@ export class RegistroEmpresaComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-<<<<<<< HEAD
     $('#scroll').click(function (e) {
       e.preventDefault();		//evitar el eventos del enlace normal
       var strAncla = $(this).attr('href'); //id del ancla
       $('body,html').stop(true, true).animate({
         scrollTop: $(strAncla).offset().top - 60
-=======
-    $('#btn').click(function (e) {
-      e.preventDefault();		//evitar el eventos del enlace normal
-      var strAncla = $(this).attr('href'); //id del ancla
-      $('body,html').stop(true, true).animate({
-        scrollTop: $(strAncla).offset().top -60
->>>>>>> 20962901c9d375c2666f61e064d9c30f1c647dd5
       }, 1000);
 
     });
